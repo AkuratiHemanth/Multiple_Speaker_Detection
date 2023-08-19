b3d10fac53547485d535e4d498785d79aa35f3b3
Akurati Hemanth <129819031+AkuratiHemanth@users.noreply.github.com>
2023-08-19 16:58:17 +0530

    Update and rename Multiple_Speaker_Identification.ipynb to Multiple_Speaker_Identification.py

---
+++ Multiple_Speaker_Identification.py
@@ -0,0 +1,13 @@
+# Importing essential libraries such as TensorFlow for machine learning, file handling tools like os and shutil, numerical computations with numpy, high-level neural networks API keras, path manipulation with pathlib, content display via IPython.display, and system command execution via subprocess. 📚💻🔧
+
+import tensorflow as tf
+import os
+from os.path import isfile, join
+import numpy as np
+import shutil
+from tensorflow import keras
+from pathlib import Path
+from IPython.display import display, Audio
+import subprocess
+
+
